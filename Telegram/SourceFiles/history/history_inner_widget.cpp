29307b6e76b51ebaf367e86d2f6493cc06fee580
Saidjon <saidjon@example.com>
2026-03-15 14:17:01 +0500

    feat: add Custom Mod Settings GUI and link toggles to bypass and ghost mode logic

--- Telegram/SourceFiles/history/history_inner_widget.cpp
+++ Telegram/SourceFiles/history/history_inner_widget.cpp
@@ -8,6 +8,7 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 #include "history/history_inner_widget.h"
 
 #include "api/api_polls.h"
+#include <QtCore/QSettings>
 #include "chat_helpers/stickers_emoji_pack.h"
 #include "core/application.h"
 #include "core/file_utilities.h"
@@ -3489,12 +3490,20 @@ void HistoryInner::showContextMenu(QContextMenuEvent *e, bool showFromTouch) {
 }
 
 bool HistoryInner::hasCopyRestriction(HistoryItem *item) const {
-	return false; // CUSTOM BYPASS: Always allow copy
+	QSettings customSettings("CustomMod", "TelegramDesktop");
+	if (customSettings.value("bypass_restrictions", true).toBool()) {
+		return false; // CUSTOM BYPASS
+	}
+	return !_peer->allowsForwarding() || (item && item->forbidsForward());
 }
 
 bool HistoryInner::hasCopyMediaRestriction(
 		not_null<HistoryItem*> item) const {
-	return false; // CUSTOM BYPASS: Always allow media copy
+	QSettings customSettings("CustomMod", "TelegramDesktop");
+	if (customSettings.value("bypass_restrictions", true).toBool()) {
+		return false; // CUSTOM BYPASS
+	}
+	return hasCopyRestriction(item) || item->forbidsSaving();
 }
 
 bool HistoryInner::showCopyRestriction(HistoryItem *item) {
